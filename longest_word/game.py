33183b2e25bf31a1c3d96201de7bd18aea1705a9
Tom Brooks <tombrooks248@gmail.com>
2024-10-01 18:44:30 +0000

    Game development with TDD

---
+++ longest_word/game.py
@@ -0,0 +1,21 @@
+import string
+import random
+
+class Game:
+    def __init__(self) -> list:
+        """Attribute a random grid to size 9"""
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+
+    def is_valid(self, word: str) -> bool:
+        """Return True if and only if the word is valid, given the Game's grid"""
+
+        for input_char in word:
+            input_char_in_grid = False
+            for grid_char in self.grid:
+                if input_char == grid_char:
+                    input_char_in_grid = True
+            if input_char_in_grid == False:
+                return False
+        return True
---
+++ tests/test_game.py
@@ -0,0 +1,38 @@
+from longest_word.game import Game
+import string
+
+class TestGame:
+    def test_game_initialization(self):
+            new_game = Game()
+
+            grid = new_game.grid
+
+            assert len(grid) == 9
+            assert type(grid) == list
+            for letter in grid:
+                assert letter in string.ascii_uppercase
+            # teardown
+            pass
+
+    def test_is_valid(self):
+
+        new_game = Game()
+        test_grid = "ABEIOVET"
+        test_word_one = "BET"
+        test_word_two = "BEET"
+
+        new_game.grid = list(test_grid)
+
+        assert new_game.is_valid(test_word_one) == True
+        assert new_game.is_valid(test_word_two) == True
+
+
+    def test_is_invalid(self):
+
+        new_game = Game()
+        test_grid = "ABEIOVET"
+        test_word_three = "BATTER"
+
+        new_game.grid = list(test_grid)
+
+        assert new_game.is_valid(test_word_three) == False
